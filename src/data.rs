18d480901152e7c1379a5ba89f7c05a09b1de1e9
Robin Appelman <robin@icewind.nl>
2022-05-03 18:41:32 +0200

    split data handling from web stuff

---
+++ src/data.rs
@@ -0,0 +1,355 @@
+use crate::DropsError;
+use moka::future::Cache;
+use serde::{Deserialize, Serialize};
+use sqlx::PgPool;
+use std::collections::HashSet;
+use std::fmt;
+use std::fmt::Display;
+use std::sync::Arc;
+use std::time::Duration;
+use steamid_ng::SteamID;
+use tracing::instrument;
+
+#[derive(Clone)]
+pub struct DataSource {
+    global_cache: Cache<(), GlobalStats>,
+    top_cache: Cache<TopOrder, Arc<Vec<TopStats>>>,
+    database: PgPool,
+    api_key: String,
+}
+
+impl DataSource {
+    pub fn new(database: PgPool, api_key: String) -> Self {
+        DataSource {
+            global_cache: Cache::builder()
+                .time_to_live(Duration::from_secs(15 * 60))
+                .time_to_idle(Duration::from_secs(5 * 60))
+                .build(),
+            top_cache: Cache::builder()
+                .time_to_live(Duration::from_secs(5 * 60))
+                .time_to_idle(Duration::from_secs(60))
+                .build(),
+            database,
+            api_key,
+        }
+    }
+
+    #[instrument(skip(self))]
+    pub async fn get_user_name(&self, steam_id: SteamID) -> Result<Option<String>, DropsError> {
+        let result = sqlx::query!(
+            r#"SELECT name
+        FROM user_names
+        WHERE steam_id=$1"#,
+            steam_id.steam3()
+        )
+        .fetch_one(&self.database)
+        .await?;
+
+        Ok(result.name)
+    }
+
+    #[instrument(skip(self))]
+    pub async fn player_search(&self, search: &str) -> Result<Vec<SearchResult>, DropsError> {
+        let mut players: Vec<SearchResult> = sqlx::query_as!(
+        SearchResult,
+        r#"SELECT steam_id as "steam_id!", name as "name!", count as "count!", (1 - (name  <-> $1)) AS "sim!" 
+        FROM medic_names
+        WHERE name ~* $1
+        ORDER BY count DESC
+        LIMIT 50"#,
+        search
+    )
+            .fetch_all(&self.database)
+            .await?;
+
+        players.sort_by(|a, b| b.weight().partial_cmp(&a.weight()).unwrap());
+
+        let mut found = HashSet::new();
+
+        Ok(players
+            .into_iter()
+            .filter(|player| {
+                if !found.contains(&player.steam_id) {
+                    found.insert(player.steam_id.clone());
+                    true
+                } else {
+                    false
+                }
+            })
+            .collect())
+    }
+
+    #[instrument(skip(self))]
+    pub async fn stats_for_user(&self, steam_id: SteamID) -> Result<DropStats, DropsError> {
+        // for medics with more than 100 drops we have cached info
+        if let Ok(result) = sqlx::query_as!(
+        DropStats,
+        r#"SELECT steam_id as "steam_id!", name as "name!", games as "games!", ubers as "ubers!", drops as "drops!",
+        medic_time as "medic_time!", drops_rank as "drops_rank!", dpu_rank as "dpu_rank!", dps_rank as "dps_rank!", dpg_rank as "dpg_rank!"
+        FROM ranked_medic_stats
+        WHERE steam_id=$1"#,
+        steam_id.steam3()
+    )
+            .fetch_one(&self.database)
+            .await {
+            return Ok(result);
+        }
+
+        // for other we need to recalculate
+        let result = sqlx::query_as!(
+        DropStats,
+        r#"SELECT user_names.steam_id as "steam_id!", name as "name!", games as "games!", ubers as "ubers!", drops as "drops!", medic_time as "medic_time!",
+        (SELECT COUNT(*) FROM ranked_medic_stats m2 WHERE m2.drops > medic_stats.drops AND m2.drops > 100) + 1 AS "drops_rank!",
+        (SELECT COUNT(*) FROM ranked_medic_stats m2 WHERE m2.dpu > medic_stats.dpu AND m2.drops > 100) + 1 AS "dpu_rank!",
+        (SELECT COUNT(*) FROM ranked_medic_stats m2 WHERE m2.dps > medic_stats.dps AND m2.drops > 100) + 1 AS "dps_rank!",
+        (SELECT COUNT(*) FROM ranked_medic_stats m2 WHERE m2.dpg > medic_stats.dpg AND m2.drops > 100) + 1 AS "dpg_rank!"
+        FROM medic_stats
+        INNER JOIN user_names ON user_names.steam_id = medic_stats.steam_id
+        WHERE medic_stats.steam_id=$1"#,
+        steam_id.steam3()
+    )
+            .fetch_one(&self.database)
+            .await?;
+
+        Ok(result)
+    }
+
+    #[instrument(skip(self))]
+    pub async fn top_stats(&self, order: TopOrder) -> Result<Arc<Vec<TopStats>>, DropsError> {
+        let result = self.top_cache.try_get_with::<_, sqlx::Error>(order, async {
+            let result = match order {
+                TopOrder::Drops => {
+                    sqlx::query_as!(
+                    TopStats,
+                    r#"SELECT steam_id as "steam_id!", games as "games!", ubers as "ubers!", drops as "drops!", medic_time as "medic_time!", name as "name!"
+                    FROM ranked_medic_stats
+                    ORDER BY drops DESC LIMIT 25"#
+                )
+                        .fetch_all(&self.database)
+                        .await?
+                }
+                TopOrder::Dps => {
+                    sqlx::query_as!(
+                    TopStats,
+                    r#"SELECT steam_id as "steam_id!", games as "games!", ubers as "ubers!", drops as "drops!", medic_time as "medic_time!", name as "name!"
+                    FROM ranked_medic_stats
+                    ORDER BY dps DESC LIMIT 25"#
+                )
+                        .fetch_all(&self.database)
+                        .await?
+                }
+                TopOrder::Dpu => {
+                    sqlx::query_as!(
+                    TopStats,
+                    r#"SELECT steam_id as "steam_id!", games as "games!", ubers as "ubers!", drops as "drops!", medic_time as "medic_time!", name as "name!"
+                    FROM ranked_medic_stats
+                    ORDER BY dpu DESC LIMIT 25"#
+                )
+                        .fetch_all(&self.database)
+                        .await?
+                }
+                TopOrder::Dpg => {
+                    sqlx::query_as!(
+                    TopStats,
+                    r#"SELECT steam_id as "steam_id!", games as "games!", ubers as "ubers!", drops as "drops!", medic_time as "medic_time!", name as "name!"
+                    FROM ranked_medic_stats
+                    ORDER BY dpg DESC LIMIT 25"#
+                )
+                        .fetch_all(&self.database)
+                        .await?
+                }
+            };
+            Ok(Arc::new(result))
+        }).await?;
+
+        Ok(result)
+    }
+
+    #[instrument(skip(self))]
+    pub async fn global_stats(&self) -> Result<GlobalStats, DropsError> {
+        let result = self.global_cache
+            .try_get_with(
+                (),
+                sqlx::query_as!(
+                GlobalStats,
+                r#"SELECT drops as "drops!", ubers as "ubers!", games as "games!" FROM global_stats"#
+            )
+                    .fetch_one(&self.database),
+            )
+            .await?;
+
+        Ok(result)
+    }
+
+    #[instrument(skip(self))]
+    pub async fn resolve_vanity_url(&self, url: &str) -> Result<Option<SteamID>, DropsError> {
+        if let Ok(row) = sqlx::query!(r#"SELECT steam_id FROM vanity_urls WHERE url=$1"#, url)
+            .fetch_one(&self.database)
+            .await
+        {
+            let steam_id: String = row.steam_id;
+            Ok(Some(SteamID::from_steam3(&steam_id)?))
+        } else {
+            if let Some(steam_id) =
+                steam_resolve_vanity::resolve_vanity_url(url, &self.api_key).await?
+            {
+                sqlx::query!(
+                    r#"INSERT INTO vanity_urls(url, steam_id) VALUES($1, $2)"#,
+                    url,
+                    steam_id.steam3()
+                )
+                .execute(&self.database)
+                .await?;
+
+                Ok(Some(steam_id))
+            } else {
+                Ok(None)
+            }
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct SearchParams {
+    pub search: String,
+}
+
+#[derive(Debug, Serialize)]
+pub struct SearchResult {
+    pub steam_id: String,
+    pub name: String,
+    pub count: i64,
+    pub sim: f64,
+}
+
+impl SearchResult {
+    pub fn weight(&self) -> f64 {
+        self.sim * 5.0 + self.count as f64 * 1.0
+    }
+}
+
+#[derive(Debug)]
+pub struct DropStats {
+    pub steam_id: String,
+    pub name: String,
+    pub drops: i64,
+    pub ubers: i64,
+    pub games: i64,
+    pub medic_time: i64,
+    pub drops_rank: i64,
+    pub dpu_rank: i64,
+    pub dps_rank: i64,
+    pub dpg_rank: i64,
+}
+
+impl DropStats {
+    pub fn dpm(&self) -> String {
+        format!(
+            "{:.2}",
+            self.drops as f64 / (self.medic_time as f64 / 3600.0)
+        )
+    }
+
+    pub fn dpu(&self) -> String {
+        format!("{:.2}", self.drops as f64 / self.ubers as f64)
+    }
+
+    pub fn dpg(&self) -> String {
+        format!("{:.2}", self.drops as f64 / self.games as f64)
+    }
+
+    pub fn steam_link(&self) -> String {
+        format!("https://steamcommunity.com/profiles/{}", self.steam_id)
+    }
+
+    pub fn etf2l_link(&self) -> String {
+        format!(
+            "http://etf2l.org/search/{}",
+            &self.steam_id[1..self.steam_id.len() - 1]
+        )
+    }
+
+    pub fn ugc_link(&self) -> String {
+        let steam_id_64 = u64::from(SteamID::from_steam3(&self.steam_id).unwrap());
+
+        format!(
+            "https://www.ugcleague.com/players_page.cfm?player_id={}",
+            steam_id_64
+        )
+    }
+
+    pub fn logs_link(&self) -> String {
+        let steam_id_64 = u64::from(SteamID::from_steam3(&self.steam_id).unwrap());
+
+        format!("http://logs.tf/profile/{}", steam_id_64)
+    }
+
+    pub fn demos_link(&self) -> String {
+        let steam_id_64 = u64::from(SteamID::from_steam3(&self.steam_id).unwrap());
+
+        format!("http://demos.tf/profiles/{}", steam_id_64)
+    }
+
+    pub fn rgl_link(&self) -> String {
+        let steam_id_64 = u64::from(SteamID::from_steam3(&self.steam_id).unwrap());
+
+        format!("https://rgl.gg/Public/PlayerProfile.aspx?p={}", steam_id_64)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct TopStats {
+    pub steam_id: String,
+    pub name: String,
+    pub drops: i64,
+    pub ubers: i64,
+    pub games: i64,
+    pub medic_time: i64,
+}
+
+impl TopStats {
+    pub fn dpm(&self) -> String {
+        format!(
+            "{:.2}",
+            self.drops as f64 / (self.medic_time as f64 / 3600.0)
+        )
+    }
+
+    pub fn dpu(&self) -> String {
+        format!("{:.2}", self.drops as f64 / self.ubers as f64)
+    }
+
+    pub fn dpg(&self) -> String {
+        format!("{:.2}", self.drops as f64 / self.games as f64)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct GlobalStats {
+    pub drops: i64,
+    pub ubers: i64,
+    pub games: i64,
+}
+
+#[derive(Debug, Hash, Eq, PartialEq, Clone, Copy)]
+pub enum TopOrder {
+    Drops,
+    Dps,
+    Dpg,
+    Dpu,
+}
+
+impl Display for TopOrder {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                TopOrder::Drops => "drops",
+                TopOrder::Dps => "dps",
+                TopOrder::Dpg => "dpg",
+                TopOrder::Dpu => "dpu",
+            }
+        )
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,22 @@
-use crate::search::api_search;
+use crate::data::{
+    DataSource, DropStats, GlobalStats, SearchParams, SearchResult, TopOrder, TopStats,
+};
 use askama::Template;
 use main_error::MainError;
-use moka::future::Cache;
 use sqlx::postgres::PgPool;
 use std::convert::TryInto;
 use std::error::Error;
 use std::fmt::{self, Debug, Display};
 use std::str::FromStr;
-use std::sync::Arc;
-use std::time::Duration;
-use steamid_ng::SteamID;
 use tracing::instrument;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 use warp::reject::Reject;
 use warp::{Filter, Rejection, Reply};
 
-mod search;
+mod data;
 
-struct DropsError(Box<dyn Error + Send + Sync + 'static>);
+pub struct DropsError(Box<dyn Error + Send + Sync + 'static>);
 
 impl<E: Into<Box<dyn Error + Send + Sync + 'static>>> From<E> for DropsError {
     fn from(e: E) -> Self {
@@ -38,218 +36,6 @@ impl Debug for DropsError {
         }
         Ok(())
     }
-}
-
-#[derive(Debug)]
-struct DropStats {
-    steam_id: String,
-    name: String,
-    drops: i64,
-    ubers: i64,
-    games: i64,
-    medic_time: i64,
-    drops_rank: i64,
-    dpu_rank: i64,
-    dps_rank: i64,
-    dpg_rank: i64,
-}
-
-impl DropStats {
-    pub fn dpm(&self) -> String {
-        format!(
-            "{:.2}",
-            self.drops as f64 / (self.medic_time as f64 / 3600.0)
-        )
-    }
-
-    pub fn dpu(&self) -> String {
-        format!("{:.2}", self.drops as f64 / self.ubers as f64)
-    }
-
-    pub fn dpg(&self) -> String {
-        format!("{:.2}", self.drops as f64 / self.games as f64)
-    }
-
-    pub fn steam_link(&self) -> String {
-        format!("https://steamcommunity.com/profiles/{}", self.steam_id)
-    }
-
-    pub fn etf2l_link(&self) -> String {
-        format!(
-            "http://etf2l.org/search/{}",
-            &self.steam_id[1..self.steam_id.len() - 1]
-        )
-    }
-
-    pub fn ugc_link(&self) -> String {
-        let steam_id_64 = u64::from(SteamID::from_steam3(&self.steam_id).unwrap());
-
-        format!(
-            "https://www.ugcleague.com/players_page.cfm?player_id={}",
-            steam_id_64
-        )
-    }
-
-    pub fn logs_link(&self) -> String {
-        let steam_id_64 = u64::from(SteamID::from_steam3(&self.steam_id).unwrap());
-
-        format!("http://logs.tf/profile/{}", steam_id_64)
-    }
-
-    pub fn demos_link(&self) -> String {
-        let steam_id_64 = u64::from(SteamID::from_steam3(&self.steam_id).unwrap());
-
-        format!("http://demos.tf/profiles/{}", steam_id_64)
-    }
-
-    pub fn rgl_link(&self) -> String {
-        let steam_id_64 = u64::from(SteamID::from_steam3(&self.steam_id).unwrap());
-
-        format!("https://rgl.gg/Public/PlayerProfile.aspx?p={}", steam_id_64)
-    }
-}
-
-#[instrument(skip(database))]
-async fn stats_for_user(steam_id: SteamID, database: &PgPool) -> Result<DropStats, DropsError> {
-    // for medics with more than 100 drops we have cached info
-    if let Ok(result) = sqlx::query_as!(
-        DropStats,
-        r#"SELECT steam_id as "steam_id!", name as "name!", games as "games!", ubers as "ubers!", drops as "drops!",
-        medic_time as "medic_time!", drops_rank as "drops_rank!", dpu_rank as "dpu_rank!", dps_rank as "dps_rank!", dpg_rank as "dpg_rank!"
-        FROM ranked_medic_stats
-        WHERE steam_id=$1"#,
-        steam_id.steam3()
-    )
-        .fetch_one(database)
-        .await {
-        return Ok(result);
-    }
-
-    // for other we need to recalculate
-    let result = sqlx::query_as!(
-        DropStats,
-        r#"SELECT user_names.steam_id as "steam_id!", name as "name!", games as "games!", ubers as "ubers!", drops as "drops!", medic_time as "medic_time!",
-        (SELECT COUNT(*) FROM ranked_medic_stats m2 WHERE m2.drops > medic_stats.drops AND m2.drops > 100) + 1 AS "drops_rank!",
-        (SELECT COUNT(*) FROM ranked_medic_stats m2 WHERE m2.dpu > medic_stats.dpu AND m2.drops > 100) + 1 AS "dpu_rank!",
-        (SELECT COUNT(*) FROM ranked_medic_stats m2 WHERE m2.dps > medic_stats.dps AND m2.drops > 100) + 1 AS "dps_rank!",
-        (SELECT COUNT(*) FROM ranked_medic_stats m2 WHERE m2.dpg > medic_stats.dpg AND m2.drops > 100) + 1 AS "dpg_rank!"
-        FROM medic_stats
-        INNER JOIN user_names ON user_names.steam_id = medic_stats.steam_id
-        WHERE medic_stats.steam_id=$1"#,
-        steam_id.steam3()
-    )
-        .fetch_one(database)
-        .await?;
-
-    Ok(result)
-}
-
-#[derive(Debug, Clone)]
-struct TopStats {
-    steam_id: String,
-    name: String,
-    drops: i64,
-    ubers: i64,
-    games: i64,
-    medic_time: i64,
-}
-
-impl TopStats {
-    pub fn dpm(&self) -> String {
-        format!(
-            "{:.2}",
-            self.drops as f64 / (self.medic_time as f64 / 3600.0)
-        )
-    }
-
-    pub fn dpu(&self) -> String {
-        format!("{:.2}", self.drops as f64 / self.ubers as f64)
-    }
-
-    pub fn dpg(&self) -> String {
-        format!("{:.2}", self.drops as f64 / self.games as f64)
-    }
-}
-
-#[instrument(skip(database, cache))]
-async fn top_stats(
-    database: &PgPool,
-    order: TopOrder,
-    cache: Cache<TopOrder, Arc<Vec<TopStats>>>,
-) -> Result<Arc<Vec<TopStats>>, DropsError> {
-    let result = cache.try_get_with::<_, sqlx::Error>(order, async {
-        let result = match order {
-            TopOrder::Drops => {
-                sqlx::query_as!(
-                    TopStats,
-                    r#"SELECT steam_id as "steam_id!", games as "games!", ubers as "ubers!", drops as "drops!", medic_time as "medic_time!", name as "name!"
-                    FROM ranked_medic_stats
-                    ORDER BY drops DESC LIMIT 25"#
-                )
-                    .fetch_all(database)
-                    .await?
-            }
-            TopOrder::Dps => {
-                sqlx::query_as!(
-                    TopStats,
-                    r#"SELECT steam_id as "steam_id!", games as "games!", ubers as "ubers!", drops as "drops!", medic_time as "medic_time!", name as "name!"
-                    FROM ranked_medic_stats
-                    ORDER BY dps DESC LIMIT 25"#
-                )
-                    .fetch_all(database)
-                    .await?
-            }
-            TopOrder::Dpu => {
-                sqlx::query_as!(
-                    TopStats,
-                    r#"SELECT steam_id as "steam_id!", games as "games!", ubers as "ubers!", drops as "drops!", medic_time as "medic_time!", name as "name!"
-                    FROM ranked_medic_stats
-                    ORDER BY dpu DESC LIMIT 25"#
-                )
-                    .fetch_all(database)
-                    .await?
-            }
-            TopOrder::Dpg => {
-                sqlx::query_as!(
-                    TopStats,
-                    r#"SELECT steam_id as "steam_id!", games as "games!", ubers as "ubers!", drops as "drops!", medic_time as "medic_time!", name as "name!"
-                    FROM ranked_medic_stats
-                    ORDER BY dpg DESC LIMIT 25"#
-                )
-                    .fetch_all(database)
-                    .await?
-            }
-        };
-        Ok(Arc::new(result))
-    }).await?;
-
-    Ok(result)
-}
-
-#[derive(Debug, Clone)]
-struct GlobalStats {
-    drops: i64,
-    ubers: i64,
-    games: i64,
-}
-
-#[instrument(skip(database, cache))]
-async fn global_stats(
-    database: &PgPool,
-    cache: Cache<(), GlobalStats>,
-) -> Result<GlobalStats, DropsError> {
-    let result = cache
-        .try_get_with(
-            (),
-            sqlx::query_as!(
-                GlobalStats,
-                r#"SELECT drops as "drops!", ubers as "ubers!", games as "games!" FROM global_stats"#
-            )
-                .fetch_one(database),
-        )
-        .await?;
-
-    Ok(result)
 }
 
 #[derive(Template)]
@@ -273,38 +59,10 @@ struct NotFoundTemplate;
 #[template(path = "404.html")]
 struct PageNotFoundTemplate;
 
-#[derive(Debug, Hash, Eq, PartialEq, Clone, Copy)]
-enum TopOrder {
-    Drops,
-    Dps,
-    Dpg,
-    Dpu,
-}
-
-impl Display for TopOrder {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                TopOrder::Drops => "drops",
-                TopOrder::Dps => "dps",
-                TopOrder::Dpg => "dpg",
-                TopOrder::Dpu => "dpu",
-            }
-        )
-    }
-}
-
-#[instrument(skip(pool, global_cache, top_cache))]
-async fn page_top_stats(
-    pool: PgPool,
-    order: TopOrder,
-    top_cache: Cache<TopOrder, Arc<Vec<TopStats>>>,
-    global_cache: Cache<(), GlobalStats>,
-) -> Result<impl Reply, Rejection> {
-    let top = top_stats(&pool, order, top_cache).await?;
-    let stats = global_stats(&pool, global_cache).await?;
+#[instrument(skip(data_source))]
+async fn page_top_stats(data_source: DataSource, order: TopOrder) -> Result<impl Reply, Rejection> {
+    let top = data_source.top_stats(order).await?;
+    let stats = data_source.global_stats().await?;
     let template = IndexTemplate {
         top: top.as_slice(),
         stats,
@@ -312,48 +70,13 @@ async fn page_top_stats(
     Ok(warp::reply::html(template.render().unwrap()))
 }
 
-#[instrument(skip(database, api_key))]
-async fn resolve_vanity_url(
-    database: &PgPool,
-    url: &str,
-    api_key: &str,
-) -> Result<Option<SteamID>, DropsError> {
-    if let Ok(row) = sqlx::query!(r#"SELECT steam_id FROM vanity_urls WHERE url=$1"#, url)
-        .fetch_one(database)
-        .await
-    {
-        let steam_id: String = row.steam_id;
-        Ok(Some(SteamID::from_steam3(&steam_id)?))
-    } else {
-        if let Some(steam_id) = steam_resolve_vanity::resolve_vanity_url(url, api_key).await? {
-            sqlx::query!(
-                r#"INSERT INTO vanity_urls(url, steam_id) VALUES($1, $2)"#,
-                url,
-                steam_id.steam3()
-            )
-            .execute(database)
-            .await?;
-
-            Ok(Some(steam_id))
-        } else {
-            Ok(None)
-        }
-    }
-}
-
-#[instrument(skip(pool, api_key))]
-async fn page_player(
-    steam_id: String,
-    pool: PgPool,
-    api_key: String,
-) -> Result<impl Reply, Rejection> {
+#[instrument(skip(data_source))]
+async fn page_player(data_source: DataSource, steam_id: String) -> Result<impl Reply, Rejection> {
     let steam_id = match steam_id.as_str().try_into().map_err(DropsError::from) {
         Ok(steam_id) => steam_id,
-        Err(e) => resolve_vanity_url(&pool, &steam_id, &api_key)
-            .await?
-            .ok_or(e)?,
+        Err(e) => data_source.resolve_vanity_url(&steam_id).await?.ok_or(e)?,
     };
-    let stats = match stats_for_user(steam_id, &pool).await {
+    let stats = match data_source.stats_for_user(steam_id).await {
         Ok(stats) => stats,
         Err(_) => {
             let template = NotFoundTemplate;
@@ -362,6 +85,25 @@ async fn page_player(
     };
     let template = PlayerTemplate { stats };
     Ok(warp::reply::html(template.render().unwrap()))
+}
+
+#[instrument(skip(data_source))]
+pub async fn api_search(
+    data_source: DataSource,
+    query: SearchParams,
+) -> Result<impl Reply, Rejection> {
+    if let Ok(steam_id) = query.search.as_str().try_into() {
+        if let Some(name) = data_source.get_user_name(steam_id).await? {
+            return Ok(warp::reply::json(&vec![SearchResult {
+                steam_id: steam_id.steam3(),
+                name,
+                count: 1,
+                sim: 1.0,
+            }]));
+        }
+    }
+    let result = data_source.player_search(&query.search).await?;
+    Ok(warp::reply::json(&result))
 }
 
 #[tokio::main]
@@ -382,69 +124,39 @@ async fn main() -> Result<(), MainError> {
     let port = u16::from_str(&dotenv::var("PORT")?)?;
 
     let pool = PgPool::connect(&database_url).await?;
+    let data_source = DataSource::new(pool, api_key);
 
-    let database = warp::any().map(move || pool.clone());
-
-    let api_key = warp::any().map(move || api_key.clone());
-
-    let global_cache = Cache::builder()
-        .time_to_live(Duration::from_secs(15 * 60))
-        .time_to_idle(Duration::from_secs(5 * 60))
-        .build();
-    let global_cache = warp::any().map(move || global_cache.clone());
-
-    let top_cache = Cache::builder()
-        .time_to_live(Duration::from_secs(5 * 60))
-        .time_to_idle(Duration::from_secs(60))
-        .build();
-    let top_cache = warp::any().map(move || top_cache.clone());
+    let data_source = warp::any().map(move || data_source.clone());
 
     let index = warp::path::end()
         .and(warp::get())
-        .and(database.clone())
-        .and(top_cache.clone())
-        .and(global_cache.clone())
-        .and_then(move |pool, top_cache, global_cache| {
-            page_top_stats(pool, TopOrder::Drops, top_cache, global_cache)
-        });
+        .and(data_source.clone())
+        .and_then(move |data_source| page_top_stats(data_source, TopOrder::Drops));
 
     let dpg = warp::path::path("dpg")
         .and(warp::get())
-        .and(database.clone())
-        .and(top_cache.clone())
-        .and(global_cache.clone())
-        .and_then(move |pool, top_cache, global_cache| {
-            page_top_stats(pool, TopOrder::Dpg, top_cache, global_cache)
-        });
+        .and(data_source.clone())
+        .and_then(move |data_source| page_top_stats(data_source, TopOrder::Dpg));
 
     let dps = warp::path::path("dph")
         .and(warp::get())
-        .and(database.clone())
-        .and(top_cache.clone())
-        .and(global_cache.clone())
-        .and_then(move |pool, top_cache, global_cache| {
-            page_top_stats(pool, TopOrder::Dps, top_cache, global_cache)
-        });
+        .and(data_source.clone())
+        .and_then(move |data_source| page_top_stats(data_source, TopOrder::Dps));
 
     let dpu = warp::path::path("dpu")
         .and(warp::get())
-        .and(database.clone())
-        .and(top_cache.clone())
-        .and(global_cache.clone())
-        .and_then(move |pool, top_cache, global_cache| {
-            page_top_stats(pool, TopOrder::Dpu, top_cache, global_cache)
-        });
+        .and(data_source.clone())
+        .and_then(move |data_source| page_top_stats(data_source, TopOrder::Dpu));
 
     let player = warp::path!("profile" / String)
         .and(warp::get())
-        .and(database.clone())
-        .and(api_key.clone())
-        .and_then(page_player);
+        .and(data_source.clone())
+        .and_then(|steam_id, data_source| page_player(data_source, steam_id));
 
     let search = warp::path!("search")
         .and(warp::get())
+        .and(data_source.clone())
         .and(warp::query())
-        .and(database.clone())
         .and_then(api_search);
 
     let not_found = warp::any().map(|| {
